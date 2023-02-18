d4142f58166d9d87267fd1a16049274b01a4743d
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-02-17 21:20:01 -0700

    firstscript added

---
+++ firstscript.js
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+var sum = addNumbers(10, 20);
+console.log('Sum of 2 number ' + sum);
---
+++ firstscript.ts
@@ -0,0 +1,6 @@
+function addNumbers(a : number, b: number) {
+ return a + b;
+}
+
+let sum: number = addNumbers(10, 20);
+console.log('Sum of 2 number ' + sum)
