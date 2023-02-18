669b65932525ffb76eadbc5b9c416939ca630498
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-02-17 22:41:53 -0700

    added variables

---
+++ updateVariables.js
@@ -0,0 +1,6 @@
+var x = 100;
+console.log(x);
+var x = 200;
+console.log(x);
+var x = "My Name is Mustafa";
+console.log(x);
--- updateVariables.ts
+++ updateVariables.ts
@@ -1,6 +1,18 @@
-var x = 100;
-console.log(x);
+// var x = 100;
+// console.log(x);
 
-var x = 200;
-console.log(x);
+// var x = 200;
+// console.log(x);
+
+
+
+// var x: string = "My Name is Mustafa";
+// console.log(x);
+
+// let x = 100;
+
+// let x = 20;
+
+const a = 100;
+
 
