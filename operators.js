c4483912b5d919b62b49ae5c2e919d28b84af831
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-02-20 17:21:37 -0700

    operators added

---
+++ operators.js
@@ -0,0 +1,9 @@
+var x = 10;
+var y = 20;
+// Artihmetic Operators + = * / % ** ++ --
+console.log(x + y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(Math.pow(5, 2)); // 25
---
+++ operators.ts
@@ -0,0 +1,20 @@
+var x = 10;
+var y = 20;
+
+// Artihmetic Operators + = * / % ** ++ --
+console.log(x+y);
+console.log(y-x);
+console.log(x * y);
+console.log(x/y);
+console.log(x%y);
+console.log(5**2); // 25
+
+// Logical Operators 
+// && || !
+
+
+
+
+
+
+
